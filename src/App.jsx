85f75d4ff13ddffaf033015b0486189b6f9528ba
groverush <groverush@gmail.com>
2023-06-18 21:38:25 -0500

    fix the responsiveness for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Hero, Summary, Footer } from "./components"
 const App = () => {
   return (
     <div className="h-screen w-screen min-w-[375px] font-hanken flex flex-col md:items-center justify-between md:pt-[150px]">
-      <div className=" flex-row justify-center items-center w-full max-w-[420px] md:max-w-none mx-auto h-[95%] md:flex md:w-[700px] md:h-[400px] md:rounded-3xl md:justify-evenly">
+      <div className=" flex-row justify-center items-center w-full max-w-[420px] md:max-w-none mx-auto md:flex md:w-[700px] md:h-[400px] md:rounded-3xl md:justify-evenly">
         <Hero />
         <Summary />
       </div>
